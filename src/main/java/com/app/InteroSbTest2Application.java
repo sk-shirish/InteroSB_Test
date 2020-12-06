5d7bdf316999a703a472b861b006f3e8c60cd5a7
yshirishkumar@gmail.com <yshirishkumar@gmail.com>
2020-12-06 08:52:31 +0530

    Sk-5480 first test code change

---
+++ src/main/java/com/app/InteroSbTest2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteroSbTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteroSbTest2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/msg/MSG_Runner.java
@@ -0,0 +1,15 @@
+
+package com.app.msg;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MSG_Runner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("this is for git hub new i.e. Second example of the git hub");
+	}
+
+}
